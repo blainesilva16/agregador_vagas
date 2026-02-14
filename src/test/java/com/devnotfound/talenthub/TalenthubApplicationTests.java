9c1e8a75c6b103f824df81532657ed580048c304
ricsant <ricsant03@gmail.com>
2026-02-14 15:22:11 -0300

    Estrutura do Projeto Springboot

---
+++ src/test/java/com/devnotfound/talenthub/TalenthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devnotfound.talenthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalenthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
