e7380acb26f5b439b3b9e5d272777e734b6087d8
Nathan-Rodrigues-Dev-hub <nathancsr123@gmail.com>
2026-02-23 21:03:02 -0300

    Fix(Position): Ajustar e Adicionar rota de alterar para o PositionController

--- src/main/java/com/devnotfound/talenthub/controller/PositionController.java
+++ src/main/java/com/devnotfound/talenthub/controller/PositionController.java
@@ -2,6 +2,7 @@ package com.devnotfound.talenthub.controller;
 
 import com.devnotfound.talenthub.dto.PositionRequestDTO;
 import com.devnotfound.talenthub.dto.PositionResponseDTO;
+import com.devnotfound.talenthub.dto.UserRequestDTO;
 import com.devnotfound.talenthub.service.PositionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,6 +21,11 @@ public class PositionController {
     @PostMapping
     public ResponseEntity<PositionResponseDTO> create(@RequestBody PositionRequestDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(positionService.insert(dto));
+    }
+
+    @PutMapping
+    public ResponseEntity<PositionResponseDTO> update(@PathVariable Integer id, @RequestBody PositionRequestDTO dto) {
+        return ResponseEntity.ok(positionService.update(id, dto));
     }
 
     @GetMapping
--- src/main/java/com/devnotfound/talenthub/service/PositionService.java
+++ src/main/java/com/devnotfound/talenthub/service/PositionService.java
@@ -1,9 +1,15 @@
 package com.devnotfound.talenthub.service;
 
+import com.devnotfound.talenthub.constants.SystemConstants;
 import com.devnotfound.talenthub.dto.PositionRequestDTO;
 import com.devnotfound.talenthub.dto.PositionResponseDTO;
+import com.devnotfound.talenthub.dto.UserRequestDTO;
+import com.devnotfound.talenthub.dto.UserResponseDTO;
 import com.devnotfound.talenthub.entity.Position;
+import com.devnotfound.talenthub.entity.User;
+import com.devnotfound.talenthub.exception.DuplicateEmailException;
 import com.devnotfound.talenthub.exception.ResourceNotFoundException;
+import com.devnotfound.talenthub.mapper.UserMapper;
 import com.devnotfound.talenthub.repository.PositionRepository;
 import com.devnotfound.talenthub.mapper.PositionMapper;
 import lombok.RequiredArgsConstructor;
@@ -33,6 +39,17 @@ public class PositionService {
         Position position = PositionMapper.toEntity(dto);
         Position saved = positionRepository.save(position);
         return PositionMapper.toResponseDTO(saved);
+    }
+
+    public PositionResponseDTO update(Integer id, PositionRequestDTO dto) {
+        Position position = positionRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Cargo não encontrado com ID: " + id));
+
+        position.setName(dto.name());
+
+        Position updated = positionRepository.save(position);
+
+        return PositionMapper.toResponseDTO(updated);
     }
 
     public void delete(Integer id) {
