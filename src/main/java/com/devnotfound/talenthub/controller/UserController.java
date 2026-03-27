baf238a59acc662e50c8d3face4b5d3613765e38
Nathan-Rodrigues-Dev-hub <nathancsr123@gmail.com>
2026-03-27 18:41:48 -0300

    Feat: Adiciona rota de findAll de customers no controller de User

--- src/main/java/com/devnotfound/talenthub/controller/UserController.java
+++ src/main/java/com/devnotfound/talenthub/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.devnotfound.talenthub.controller;
 
+import com.devnotfound.talenthub.dto.CustomerResponseDTO;
 import com.devnotfound.talenthub.dto.ResetPasswordRequestDTO;
 import com.devnotfound.talenthub.dto.UserRequestDTO;
 import com.devnotfound.talenthub.dto.UserResponseDTO;
 import com.devnotfound.talenthub.service.UserService;
+import com.devnotfound.talenthub.service.CustomerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,45 +18,51 @@ import java.util.List;
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
-    
+
     private final UserService userService;
-    
+    private final CustomerService customerService;
+
     @GetMapping("/email/{email}")
     public ResponseEntity<UserResponseDTO> findByEmail(@PathVariable String email) {
         return ResponseEntity.ok(userService.findByEmail(email));
     }
-    
+
+    @GetMapping
+    public ResponseEntity<List<CustomerResponseDTO>> findAllCustomers() {
+        return ResponseEntity.ok(customerService.findAll());
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDTO> findById(@PathVariable Integer id) {
         return ResponseEntity.ok(userService.findById(id));
     }
-    
+
     @GetMapping("/")
     public ResponseEntity<List<UserResponseDTO>> findAll() {
         return ResponseEntity.ok(userService.findAll());
     }
-    
+
     @GetMapping("/search")
     public ResponseEntity<List<UserResponseDTO>> findByName(@RequestParam String name) {
         return ResponseEntity.ok(userService.findByName(name));
     }
-    
+
     @PostMapping
     public ResponseEntity<UserResponseDTO> insert(@RequestBody UserRequestDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.insert(dto));
     }
-    
+
     @PutMapping("/{id}")
     public ResponseEntity<UserResponseDTO> update(@PathVariable Integer id, @RequestBody UserRequestDTO dto) {
         return ResponseEntity.ok(userService.update(id, dto));
     }
-    
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
-    
+
     @PatchMapping("/{id}/password")
     public ResponseEntity<Void> updatePassword(@PathVariable Integer id, @RequestBody String newPassword) {
         userService.updatePassword(id, newPassword);
