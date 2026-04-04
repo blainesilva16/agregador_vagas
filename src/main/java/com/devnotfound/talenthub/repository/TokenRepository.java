6abe65a60b823c88386fafa61d0b5fa70d4b23de
ricsant <ricsant03@gmail.com>
2026-04-04 17:09:15 -0300

    ajustes para subida no render

--- src/main/java/com/devnotfound/talenthub/repository/TokenRepository.java
+++ src/main/java/com/devnotfound/talenthub/repository/TokenRepository.java
@@ -2,7 +2,9 @@ package com.devnotfound.talenthub.repository;
 
 import com.devnotfound.talenthub.entity.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TokenRepository extends JpaRepository<Token, Integer> {
 
     boolean existsByToken(String token);
